9089e5906856eb3f72f76addc66ceefcfe77b94b
Silas Huereca <silashuereca.dev@gmail.com>
2022-10-20 19:13:22 -0500

    feat: pinia store setup

---
+++ components/StarterKit.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p v-text="message" />
+    <p v-text="doubleValue" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { useCounterStore } from "../store/index.js";
+export default defineComponent({
+  setup() {
+    const store: any = useCounterStore();
+    const message: string = "Nuxt Starter Kit";
+    setInterval(() => {
+      store.increment();
+    }, 1000);
+    return {
+      message,
+      doubleValue: computed(() => store.doubleCount),
+    };
+  },
+});
+</script>
+
+<style scoped>
+p {
+  font-size: 40px;
+}
+</style>
